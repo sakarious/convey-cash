85667f22c25bbfbcfe4c351e5c71dca9e9111238
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-07-15 11:33:40 +0100

    tests -transfer

--- tests/Feature/ConveyCashApiTest.php
+++ tests/Feature/ConveyCashApiTest.php
@@ -401,6 +401,43 @@ class ConveyCashApiTest extends TestCase
     }
 
 
+    /**
+     * User cannot make transfer if bank code is invalid
+     *
+     * 
+     */
+    public function test_will_not_make_transfer_if_bank_code_is_invalid()
+    {
+        $user = User::create([
+            'name' => 'Oluwashegs',
+            'email' => 'o@gmail.com',
+            'password' => '12345'
+        ]);
+
+        $token = $user->createToken('TestToken')->accessToken;
+
+        $header = [];
+        $header['Accept'] = 'application/json';
+        $header['Authorization'] = 'Bearer '.$token;
+
+
+        $account = [
+            'account_number' => '2111333996',
+            'bank_code' => '066',
+            'amount' => '50000000',
+            'reason' => 'Laulau'
+        ];
+        
+        $response = $this->json('POST', '/api/v1/transfer', $account, $header);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            "status" => false,
+            "message" => "Unknown bank code: 066"
+            ]);
+    }
+
+
 
 
 
