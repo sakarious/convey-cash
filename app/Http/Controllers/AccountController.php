bfb55f41894c160778f1a8fd23f1e7de6b2c84dc
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-07-15 11:05:54 +0100

    account controller

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Accounts;
+use Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AccountController extends Controller
+{
+    
+}
