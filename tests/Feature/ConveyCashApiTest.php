1db76b20e5f836e10d3883efe24d8008ec86b1e4
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-07-15 11:21:43 +0100

    tests

---
+++ tests/Feature/ConveyCashApiTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Accounts;
+use App\Models\User;
+
+class ConveyCashApiTest extends TestCase
+{
+
+}
