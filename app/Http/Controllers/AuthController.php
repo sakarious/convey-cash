bc2e0f169299617c85ef789a15d2f9c028304c5a
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-07-15 10:53:44 +0100

    auth controller: login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -57,4 +57,62 @@ class AuthController extends Controller
             return response()->json($response, 201);
 
     }
+
+    //Login
+    public function login(Request $request){
+
+         //Validation rules
+         $rules = [
+            'email' =>'email|required',
+            'password' => 'required'
+        ];
+
+        //Check Validation
+        $validator = Validator::make($request->all(), $rules);
+
+        //If validation fails, Send appropriate response
+        if($validator->fails()){
+            $response = [
+                "message" => "Validation Failed",
+                "errors" => $validator->errors()
+            ];
+
+            return response()->json($response, 400);
+        }
+
+        //Login user
+        $email = $request->input('email');
+        $pass = $request->input('password');
+
+        $loginData = [
+            'email' => $email,
+            'password' => $pass
+        ];
+
+        //If login failed
+        if(!auth()->attempt($loginData)){
+            $response = [
+                'status' => 'Failed',
+                'message' => 'Username or Password Incorrect'
+            ];
+
+            return response()->json($response, 200);
+        }
+
+        //If login is successful, generate access token for user
+        $user = auth()->user()->makehidden(['updated_at', 'created_at', 'email_verified_at']);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        $response = [
+            'status' => 'Success',
+            'data' => [
+                'user' => $user,
+                'accessToken' => $accessToken
+            ]
+            ];
+
+            return response()->json($response, 200);
+
+    }
 }
